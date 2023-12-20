58d3a8d3b3a4cf58efb0877532bd78bef0db3dc7
Max Wiseman <60909484+maxwiseman@users.noreply.github.com>
2023-12-20 11:38:40 -0500

    chore: updated result text slightly

--- src/app/result.tsx
+++ src/app/result.tsx
@@ -9,9 +9,9 @@ export default function Result({
     return <>Nothing yet!</>;
   } else if (weather.main.temp <= 36)
     return <div>Its freezing, wear something warm</div>;
-  else if (weather.main.temp <= 50)
+  else if (weather.main.temp <= 55)
     return <div>Its a little cold, wear a sweater or something</div>;
-  else if (weather.main.temp <= 70)
-    return <div>Its a little cold, wear a sweater or something</div>;
+  else if (weather.main.temp <= 100)
+    return <div>Its not that cold, you'll be fine with short sleeves</div>;
   return <>Nothing yet!</>;
 }
